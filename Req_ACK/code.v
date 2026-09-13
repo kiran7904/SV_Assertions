268ab320d125f0531f6bd3c39bfe83d6b94d54fb
kiran_kumar_siripurapu <148669920+kiran7904@users.noreply.github.com>
2026-09-13 19:32:54 +0530

    Create code.v

---
+++ Req_ACK/code.v
@@ -0,0 +1,50 @@
+module handshake (
+    input  logic clk,
+    input  logic req,
+    output logic ack
+);
+
+    always_ff @(posedge clk) begin
+        ack <= req;
+    end
+
+    // Assertion
+    assert property (
+        @(posedge clk)
+        req |-> ack
+    );
+
+endmodule
+
+
+module tb;
+
+    logic clk;
+    logic req;
+    logic ack;
+
+    handshake dut (
+        .clk(clk),
+        .req(req),
+        .ack(ack)
+    );
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        req = 0;
+
+        #10;
+        req = 1;
+
+        #10;
+        req = 0;
+
+        #20;
+        $finish;
+    end
+
+endmodule
